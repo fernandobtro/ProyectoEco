89305d0c59825bcb68a75fe4bdbbf186f730aa2d
fernandobtro <xavier.gn.bn@gmail.com>
2026-02-27 18:23:05 -0600

    feat: initial architecture setup with clean modular structure

---
+++ Eco/App/EcoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoApp.swift
+//  Eco
+//
+//  Created by Fernando Buenrostro on 26/02/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
